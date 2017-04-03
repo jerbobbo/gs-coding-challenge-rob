3ca67df0d581956f243dda39d49b5dc6753be752
Eric P. Katz <ericpkatz@gmail.com>
2017-04-03 16:41:38 -0400

    use revealing module pattern

--- browser/CategoryFactory.js
+++ browser/CategoryFactory.js
@@ -1,11 +1,17 @@
 angular.module('gs').factory('CategoryFactory', function($http){
+  var findAll = function(){
+    return $http.get('/api/categories')
+      .then( function(response){ return response.data });
+  };
+
+  var findOne = function(id){
+    return 'TODO - show details for category ' + id;
+  }
+
+  var _serviceCount = { total: 0 };
   return {
-    findAll: function(){
-      return $http.get('/api/categories')
-        .then( function(response){ return response.data });
-    },
-    findOne: function(id){
-      return 'TODO - show details for category ' + id;
-    },
+    serviceCount: _serviceCount,
+    findAll: findAll,
+    findOne: findOne
   };
 });
